0f230d353ce168cda9efd6c461b43d2aadb5837c
IUsername <benjamin.v.palmer@gmail.com>
2019-03-05 17:01:46 -0700

    Added a Matrix type.

---
+++ Octans.Test/MatrixTest.cs
@@ -0,0 +1,200 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Octans.Test
+{
+    public class MatrixTest
+    {
+        [Fact]
+        public void Create4x4()
+        {
+            var m = new Matrix(
+                new[] {1.0, 2, 3, 4},
+                new[] {5.5, 6.5, 7.5, 8.5},
+                new[] {9.0, 10, 11, 12},
+                new[] {13.5, 14.5, 15.5, 16.5});
+            m[0, 0].Should().BeApproximately(1, 0.00001);
+            m[0, 3].Should().BeApproximately(4, 0.00001);
+            m[1, 0].Should().BeApproximately(5.5, 0.00001);
+            m[1, 2].Should().BeApproximately(7.5, 0.00001);
+            m[2, 2].Should().BeApproximately(11, 0.00001);
+            m[3, 0].Should().BeApproximately(13.5, 0.00001);
+            m[3, 2].Should().BeApproximately(15.5, 0.00001);
+        }
+
+        [Fact]
+        public void Create2x2()
+        {
+            var m = new Matrix(new[] {-3.0, 5.0}, new[] {1.0, -2.0});
+            m[0, 0].Should().BeApproximately(-3, 0.00001);
+            m[0, 1].Should().BeApproximately(5, 0.00001);
+            m[1, 0].Should().BeApproximately(1, 0.00001);
+            m[1, 1].Should().BeApproximately(-2, 0.00001);
+        }
+
+        [Fact]
+        public void Create3x3()
+        {
+            var m = Matrix.Square(-3, 5, 0, 1, -2, -7, 0, 1, 1);
+            m[0, 0].Should().BeApproximately(-3, 0.00001);
+            m[1, 1].Should().BeApproximately(-2, 0.00001);
+            m[2, 2].Should().BeApproximately(1, 0.00001);
+        }
+
+        [Fact]
+        public void IdenticalEquality()
+        {
+            var a = Matrix.Square(1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2);
+            var b = Matrix.Square(1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2);
+            a.Should().BeEquivalentTo(b);
+        }
+
+        [Fact]
+        public void NonEquality()
+        {
+            var a = Matrix.Square(1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2);
+            var b = Matrix.Square(2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2, 1);
+            (a == b).Should().BeFalse();
+        }
+
+        [Fact]
+        public void MultiplyMatrices()
+        {
+            var a = Matrix.Square(1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2);
+            var b = Matrix.Square(-2, 1, 2, 3, 3, 2, 1, -1, 4, 3, 6, 5, 1, 2, 7, 8);
+            (a * b).Should()
+                   .BeEquivalentTo(Matrix.Square(20, 22, 50, 48, 44, 54, 114, 108, 40, 58, 110, 102, 16, 26, 46, 42));
+        }
+
+        [Fact]
+        public void MultipleMatrixAndTuple()
+        {
+            var a = Matrix.Square(1, 2, 3, 4, 2, 4, 4, 2, 8, 6, 4, 1, 0, 0, 0, 1);
+            var b = new Tuple(1, 2, 3, 1);
+            (a * b).Should().BeEquivalentTo(new Tuple(18, 24, 33, 1));
+        }
+
+        [Fact]
+        public void MatrixMultipliedByIdentityIsMatrix()
+        {
+            var a = Matrix.Square(1, 2, 3, 4, 1, 2, 4, 8, 2, 4, 8, 16, 4, 8, 16, 32);
+            (a * Matrix.Identity4).Should().BeEquivalentTo(a);
+        }
+
+        [Fact]
+        public void TupleMultipliedByIdentityMatrixIsTuple()
+        {
+            var a = new Tuple(1, 2, 3, 4);
+            (Matrix.Identity4 * a).Should().BeEquivalentTo(a);
+        }
+
+        [Fact]
+        public void CanTranspose()
+        {
+            var m = Matrix.Square(0, 9, 3, 0, 9, 8, 0, 8, 1, 8, 5, 3, 0, 0, 5, 8);
+            m.Transpose().Should().BeEquivalentTo(Matrix.Square(0, 9, 1, 0, 9, 8, 8, 0, 3, 0, 5, 5, 0, 8, 3, 8));
+        }
+
+        [Fact]
+        public void DeterminantOf2x2()
+        {
+            var m = Matrix.Square(1, 5, -3, 2);
+            Matrix.Determinant(m).Should().Be(17);
+        }
+
+        [Fact]
+        public void Submatrix3x3()
+        {
+            var a = Matrix.Square(1, 5, 0, -3, 2, 7, 0, 6, -3);
+            Matrix.Submatrix(a, 0, 2).Should().BeEquivalentTo(Matrix.Square(-3, 2, 0, 6));
+        }
+
+        [Fact]
+        public void Submatrix4x4()
+        {
+            var a = Matrix.Square(-6, 1, 1, 6, -8, 5, 8, 6, -1, 0, 8, 2, -7, 1, -1, 1);
+            Matrix.Submatrix(a, 2, 1).Should().BeEquivalentTo(Matrix.Square(-6, 1, 6, -8, 8, 6, -7, -1, 1));
+        }
+
+        [Fact]
+        public void MinorOf3x3()
+        {
+            var a = Matrix.Square(3, 5, 0, 2, -1, -7, 6, -1, 5);
+            Matrix.Minor(a, 1, 0).Should().Be(25);
+        }
+
+        [Fact]
+        public void CofactorOf3x3()
+        {
+            var a = Matrix.Square(3, 5, 0, 2, -1, -7, 6, -1, 5);
+            Matrix.Cofactor(a, 0, 0).Should().Be(-12);
+            Matrix.Cofactor(a, 1, 0).Should().Be(-25);
+        }
+
+        [Fact]
+        public void DeterminantOf3x3()
+        {
+            var m = Matrix.Square(1, 2,6,-5,8,-4,2,6,4);
+            Matrix.Cofactor(m, 0, 0).Should().Be(56);
+            Matrix.Cofactor(m, 0, 1).Should().Be(12);
+            Matrix.Cofactor(m, 0, 2).Should().Be(-46);
+            Matrix.Determinant(m).Should().Be(-196);
+        }
+
+        [Fact]
+        public void DeterminantOf4x4()
+        {
+            var m = Matrix.Square(-2,-8,3,5,-3,1,7,3,1,2,-9,6,-6,7,7,-9);
+            Matrix.Cofactor(m, 0, 0).Should().Be(690);
+            Matrix.Cofactor(m, 0, 1).Should().Be(447);
+            Matrix.Cofactor(m, 0, 2).Should().Be(210);
+            Matrix.Cofactor(m, 0, 3).Should().Be(51);
+            Matrix.Determinant(m).Should().Be(-4071);
+        }
+
+        [Fact]
+        public void IsInvertible()
+        {
+            var m = Matrix.Square(6, 4, 4, 4, 5, 5, 7, 9, 4, -9, 3, -7, 9, 1, 7, -6);
+            Matrix.IsInvertible(m).Should().BeTrue();
+        }
+
+        [Fact]
+        public void IsNotInvertible()
+        {
+            var m = Matrix.Square(-4,2,-2,-3,9,6,2,6,0,-5,1,-5,0,0,0,0);
+            Matrix.IsInvertible(m).Should().BeFalse();
+        }
+
+        [Fact]
+        public void MatrixInverse()
+        {
+            var a = Matrix.Square(8, -5, 9, 2, 7, 5, 6, 1, -6, 0, 9, 6, -3, 0, -9, -4);
+            var invA = Matrix.Inverse(a);
+            invA.Should()
+                .BeEquivalentTo(
+                    Matrix.Square(-0.15385, -0.15385, -0.28205, -0.53846,
+                                  -0.07692, 0.12308, 0.02564, 0.03077, 
+                                  0.35897, 0.35897, 0.43590, 0.92308, 
+                                  -0.69231, -0.69231,-0.76923, -1.92308));
+
+            var b = Matrix.Square(9, 3, 0, 9, -5, -2, -6, -3, -4, 9, 6, 4, -7, 6, 6, 2);
+            var invB = Matrix.Inverse(b);
+            invB.Should()
+                .BeEquivalentTo(
+                    Matrix.Square(-0.04074, -0.07778, 0.14444, -0.22222,
+                                  -0.07778, 0.03333, 0.36667, -0.33333,
+                                  -0.02901, -0.14630, -0.10926, 0.12963,
+                                  0.17778, 0.06667, -0.26667, 0.33333));
+        }
+
+        [Fact]
+        public void MultiplyProductByInverse()
+        {
+            var a = Matrix.Square(3, -9, 7, 3, 3, -8, 2, -9, -4, 4, 4, 1, -6, 5, -1, 1);
+            var b = Matrix.Square(8,2,2,2,3,-1,7,0,7,0,5,4,6,-2,0,5);
+            var c = a * b;
+            (c * Matrix.Inverse(b)).Should().BeEquivalentTo(a);
+        }
+    }
+}
---
+++ Octans/Matrix.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Octans
+{
+    public struct Matrix : IEquatable<Matrix>
+    {
+        private const double Epsilon = 0.00001;
+
+        public int Rows { get; }
+        public int Columns { get; }
+
+        private readonly double[,] _data;
+
+        public Matrix(params double[][] values)
+        {
+            Rows = values.Length;
+            Columns = values[0].Length;
+
+            _data = new double[Rows, Columns];
+            for (var row = 0; row < Rows; row++)
+            {
+                for (var col = 0; col < Columns; col++)
+                {
+                    _data[row, col] = values[row][col];
+                }
+            }
+        }
+
+        public Matrix(int rows, int columns)
+        {
+            Rows = rows;
+            Columns = columns;
+            _data = new double[Rows, Columns];
+        }
+
+        public double this[int row, int col]
+        {
+            get => _data[row, col];
+            private set => _data[row, col] = value;
+        }
+
+        public double[][] ToArray()
+        {
+            var arr = new double[Rows][];
+            for (var row = 0; row < Rows; row++)
+            {
+                var cur = new double[Columns];
+                for (var col = 0; col < Columns; col++)
+                {
+                    cur[col] = this[row, col];
+                }
+
+                arr[row] = cur;
+            }
+
+            return arr;
+        }
+
+        public Matrix Transpose()
+        {
+            var m = new Matrix(Columns, Rows);
+            for (var row = 0; row < Rows; row++)
+            {
+                for (var col = 0; col < Columns; col++)
+                {
+                    m[col, row] = this[row, col];
+                }
+            }
+
+            return m;
+        }
+
+        public bool Equals(Matrix other) => Rows == other.Rows && Columns == other.Columns && ValuesEqual(this, other);
+
+        private static bool Equal(double a, double b) => Math.Abs(a - b) < Epsilon;
+
+        private bool ValuesEqual(Matrix a, Matrix b)
+        {
+            for (var row = 0; row < Rows; row++)
+            {
+                for (var col = 0; col < Columns; col++)
+                {
+                    if (!Equal(a[row, col], b[row, col]))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is null)
+            {
+                return false;
+            }
+
+            return obj is Matrix other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = _data != null ? _data.GetHashCode() : 0;
+                hashCode = (hashCode * 397) ^ Rows;
+                hashCode = (hashCode * 397) ^ Columns;
+                return hashCode;
+            }
+        }
+
+        public static Matrix Identity4 = new Matrix(new[] {1.0, 0, 0, 0},
+                                                    new[] {0.0, 1, 0, 0},
+                                                    new[] {0.0, 0, 1, 0},
+                                                    new[] {0.0, 0, 0, 1});
+
+        public static Matrix Square(params double[] values)
+        {
+            var len = values.Length;
+            int size;
+            switch (len)
+            {
+                case 16:
+                    size = 4;
+                    break;
+                case 9:
+                    size = 3;
+                    break;
+                case 4:
+                    size = 2;
+                    break;
+                default:
+                    size = (int) Math.Sqrt(len);
+                    break;
+            }
+
+            var arr = new double[size][];
+            var k = 0;
+            for (var row = 0; row < size; row++)
+            {
+                var cur = new double[size];
+                for (var col = 0; col < size; col++)
+                {
+                    cur[col] = values[k++];
+                }
+
+                arr[row] = cur;
+            }
+
+            return new Matrix(arr);
+        }
+
+        private static Matrix ToMatrix(Tuple t) => new Matrix(new[] {t.X}, new[] {t.Y}, new[] {t.Z}, new[] {t.W});
+
+        private static Tuple ToTuple(Matrix m) => new Tuple(m[0, 0], m[1, 0], m[2, 0], m[3, 0]);
+
+        private static Matrix Multiply(Matrix a, Matrix b)
+        {
+            if (a.Columns != b.Rows)
+            {
+                throw new InvalidOperationException("Matrices do not have the correct shapes for multiplication.");
+            }
+
+            var m = new Matrix(a.Rows, b.Columns);
+            for (var r = 0; r < a.Rows; r++)
+            {
+                for (var c = 0; c < b.Columns; c++)
+                {
+                    var temp = 0.0;
+                    for (var k = 0; k < a.Columns; k++)
+                    {
+                        temp += a[r, k] * b[k, c];
+                    }
+
+                    m[r, c] = temp;
+                }
+            }
+
+            return m;
+        }
+
+        public static double Determinant(Matrix m)
+        {
+            if (m.Columns == 2 && m.Rows == 2)
+            {
+               return m[0, 0] * m[1, 1] - m[0, 1] * m[1, 0];
+            }
+
+            var det = 0.0;
+            for (var c = 0; c < m.Columns; c++)
+            {
+                det += m[0, c] * Cofactor(m, 0, c);
+            }
+
+            return det;
+        }
+
+        public static Matrix Submatrix(Matrix m, int row, int column)
+        {
+            var arr = new double[m.Rows - 1][];
+            var rr = 0;
+            for (var r = 0; r < m.Rows; r++)
+            {
+                if (r == row)
+                {
+                    continue;
+                }
+
+                var cc = 0;
+                var cur = new double[m.Columns - 1];
+                for (var c = 0; c < m.Columns; c++)
+                {
+                    if (c == column)
+                    {
+                        continue;
+                    }
+
+                    cur[cc++] = m[r, c];
+                }
+
+                arr[rr++] = cur;
+            }
+
+            return new Matrix(arr);
+        }
+
+        public static double Minor(Matrix m, int row, int column)
+        {
+            var s = Submatrix(m, row, column);
+            return Determinant(s);
+        }
+
+        public static double Cofactor(Matrix m, int row, int column)
+        {
+            var n = Minor(m, row, column);
+            return (row + column) % 2 == 0 ? n : -n;
+        }
+
+        public static bool IsInvertible(Matrix m)
+        {
+            // ReSharper disable once CompareOfFloatsByEqualityOperator
+            return Determinant(m) != 0.0;
+        }
+
+        public static Matrix Inverse(Matrix m)
+        {
+            var det = Determinant(m);
+            // ReSharper disable once CompareOfFloatsByEqualityOperator
+            if (det == 0.0)
+            {
+                throw new InvalidOperationException("Matrix is not invertible.");
+            }
+
+            var m2 = new Matrix(m.Columns, m.Rows);
+            for (var r = 0; r < m.Rows; r++)
+            {
+                for (var c = 0; c < m.Columns; c++)
+                {
+                    var cf = Cofactor(m, r, c);
+                    // Transpose.
+                    m2[c, r] = cf / det;
+                }
+            }
+
+            return m2;
+        }
+
+        public static bool operator ==(Matrix left, Matrix right) => left.Equals(right);
+
+        public static bool operator !=(Matrix left, Matrix right) => !left.Equals(right);
+
+        public static Matrix operator *(Matrix left, Matrix right) => Multiply(left, right);
+
+        public static Tuple operator *(Matrix left, Tuple right) => ToTuple(Multiply(left, ToMatrix(right)));
+    }
+}
