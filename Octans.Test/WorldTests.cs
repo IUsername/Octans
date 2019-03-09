341279cc4cca626900857a012c55b3dd8ecea045
IUsername <benjamin.v.palmer@gmail.com>
2019-03-08 19:20:15 -0700

    Added Cylinder shape.

--- Octans.Test/WorldTests.cs
+++ Octans.Test/WorldTests.cs
@@ -30,10 +30,9 @@ namespace Octans.Test
         [Fact]
         public void TestRender()
         {
-
             var s1 = new StripePattern(Colors.White, Colors.Black);
             var s2 = new StripePattern(Colors.White, Colors.Black);
-            s2.SetTransform(Transforms.RotateY(MathF.PI/2));
+            s2.SetTransform(Transforms.RotateY(MathF.PI / 2));
             var pattern = new BlendedCompositePattern(s1, s2);
 
             var stripe = new StripePattern(new Color(0.9f, 0, 0), new Color(0.0f, 0.0f, 0.9f));
@@ -41,31 +40,60 @@ namespace Octans.Test
             var perlin = new PerlinRippleCompositePattern(stripe, 0.8f);
             perlin.SetTransform(Transforms.Scale(0.1f, 0.1f, 0.1f));
 
-            var floor = new Plane {Material =
+            var floor = new Plane
             {
-                Pattern = pattern,// new CheckerPattern(new Color(0.75f, 0.75f, 0.75f), Colors.White),
-                Specular = 0f
-            }};
+                Material =
+                {
+                    Pattern = pattern, // new CheckerPattern(new Color(0.75f, 0.75f, 0.75f), Colors.White),
+                    Specular = 0f
+                }
+            };
 
-            var middle = new Sphere {Material = { Pattern = perlin, Diffuse = 0.7f, Specular = 0.3f, Reflective = 0.4f}};
+            var middle = new Sphere {Material = {Pattern = perlin, Diffuse = 0.7f, Specular = 0.3f, Reflective = 0.4f}};
             middle.SetTransform(Transforms.Translate(-0.5f, 1f, 0.5f));
             //middle.SetTransform(Transforms.Translate(-0.5f, 0.5f, 0.5f));
 
-            var right = new Sphere {Material = { Pattern = new SolidColor(new Color(0.5f, 1f, 0.1f)), Diffuse = 0.7f, Specular = 0.3f, Reflective = 0.2f}};
+            var right = new Sphere
+            {
+                Material =
+                {
+                    Pattern = new SolidColor(new Color(0.5f, 1f, 0.1f)), Diffuse = 0.7f, Specular = 0.3f,
+                    Reflective = 0.2f
+                }
+            };
             right.SetTransform(Transforms.Translate(1.5f, 0.5f, -0.5f) * Transforms.Scale(0.5f, 0.5f, 0.5f));
 
-            var left = new Sphere {Material = { Pattern = new SolidColor(new Color(0.9f, 0.9f, 1f)), Diffuse = 0.05f, Specular = 0.9f, Transparency = 0.9f, RefractiveIndex=1.52f, Reflective = 1.4f, Ambient = 0.11f, Shininess = 300}};
+            var left = new Sphere
+            {
+                Material =
+                {
+                    Pattern = new SolidColor(new Color(0.9f, 0.9f, 1f)), Diffuse = 0.05f, Specular = 0.9f,
+                    Transparency = 0.9f, RefractiveIndex = 1.52f, Reflective = 1.4f, Ambient = 0.11f, Shininess = 300
+                }
+            };
             left.SetTransform(Transforms.Translate(-1.5f, 0.33f, -1.0f) * Transforms.Scale(0.33f, 0.33f, 0.33f));
 
-            var cube = new Cube();
-            cube.Material.Pattern = new GradientPattern(new Color(1f, 0, 0), new Color(1f, 0.8f, 0f));
+            var cube = new Cube
+            {
+                Material = {Pattern = new GradientPattern(new Color(1f, 0, 0), new Color(1f, 0.8f, 0f))}
+            };
             cube.Material.Pattern.SetTransform(Transforms.TranslateX(-0.5f).Scale(2f).RotateZ(MathF.PI / 2f));
             cube.SetTransform(Transforms.RotateY(MathF.PI / 4f).Translate(2.5f, 1f, 3.6f).Scale(1f, 1f, 1f));
 
+            var cylinder = new Cylinder
+            {
+                Minimum = 0f,
+                Maximum = 3f,
+                IsClosed = true,
+                Material = {Reflective = 0.6f, Specular = 0.8f, Diffuse = 0.4f, Ambient=0.1f, Shininess = 200}
+            };
+            cylinder.SetTransform(Transforms.Translate(-3f, 0f, 3.5f));
+            
+
             var w = new World();
             w.SetLights(new PointLight(new Point(-10, 10, -10), Colors.White));
-            //w.SetObjects(floor, middle, right, left, cube);
-            w.SetObjects(floor, cube, middle, right, left);
+            w.SetObjects(floor, cylinder, cube, middle, right, left);
+            //w.SetObjects(floor, cube, middle, right, left);
 
             var c = new Camera(300, 200, MathF.PI / 3f);
             c.SetTransform(Transforms.View(new Point(0, 1.5f, -5f), new Point(0, 1, 0), new Vector(0, 1, 0)));
---
+++ Octans/Cylinder.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Octans
+{
+    public class Cylinder : ShapeBase
+    {
+        private const float Epsilon = 0.0001f;
+
+        public Cylinder()
+        {
+            Minimum = float.NegativeInfinity;
+            Maximum = float.PositiveInfinity;
+            IsClosed = false;
+        }
+
+        public float Minimum { get; set; }
+        public float Maximum { get; set; }
+        public bool IsClosed { get; set; }
+
+        public override IReadOnlyList<Intersection> LocalIntersects(in Ray localRay)
+        {
+            var dX = localRay.Direction.X;
+            var dZ = localRay.Direction.Z;
+            var a = dX * dX + dZ * dZ;
+            var list = new List<Intersection>();
+
+            if (!Check.Within(a, 0f, Epsilon))
+            {
+                var oX = localRay.Origin.X;
+                var oZ = localRay.Origin.Z;
+                var b = 2f * oX * dX + 2f * oZ * dZ;
+                var c = oX * oX + oZ * oZ - 1f;
+                var disc = b * b - 4f * a * c;
+                if (disc < 0f)
+                {
+                    return Intersections.Empty;
+                }
+
+                var t0 = (-b - MathF.Sqrt(disc)) / (2f * a);
+                var t1 = (-b + MathF.Sqrt(disc)) / (2f * a);
+
+                if (t0 > t1)
+                {
+                    (t0, t1) = (t1, t0);
+                }
+
+
+                var y0 = localRay.Origin.Y + t0 * localRay.Direction.Y;
+                if (Minimum < y0 && y0 < Maximum)
+                {
+                    list.Add(new Intersection(t0, this));
+                }
+
+                var y1 = localRay.Origin.Y + t1 * localRay.Direction.Y;
+                if (Minimum < y1 && y1 < Maximum)
+                {
+                    list.Add(new Intersection(t1, this));
+                }
+            }
+
+            IntersectCaps(this, localRay, list);
+
+            return new Intersections(list);
+        }
+
+        private static bool CheckCap(Ray ray, float t)
+        {
+            var x = ray.Origin.X + t * ray.Direction.X;
+            var z = ray.Origin.Z + t * ray.Direction.Z;
+            var check = x * x + z * z;
+            return check <= 1f || Check.Within(check, 1f, Epsilon);
+        }
+
+        private static void IntersectCaps(Cylinder cylinder, Ray ray, ICollection<Intersection> list)
+        {
+            if (!cylinder.IsClosed || Check.Within(ray.Direction.Y, 0f, Epsilon) || list.Count == 2)
+            {
+                return;
+            }
+
+            var t = (cylinder.Minimum - ray.Origin.Y) / ray.Direction.Y;
+            if (CheckCap(ray, t))
+            {
+                list.Add(new Intersection(t, cylinder));
+            }
+
+            t = (cylinder.Maximum - ray.Origin.Y) / ray.Direction.Y;
+            if (CheckCap(ray, t) && list.Count < 2)
+            {
+                list.Add(new Intersection(t, cylinder));
+            }
+        }
+
+        public override Vector LocalNormalAt(in Point localPoint)
+        {
+            var dist = localPoint.X * localPoint.X + localPoint.Z * localPoint.Z;
+            if (dist < 1f)
+            {
+                if (localPoint.Y >= Maximum - Epsilon)
+                {
+                    return new Vector(0, 1, 0);
+                }
+
+                if (localPoint.Y <= Minimum + Epsilon)
+                {
+                    return new Vector(0, -1, 0);
+                }
+            }
+
+            return new Vector(localPoint.X, 0, localPoint.Z);
+        }
+    }
+}
